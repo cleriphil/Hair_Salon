cfdad2f01e749ab9ba110e75658acbcdbb2010be
cleri philiastides <cleri.phil@gmail.com>
2015-05-08 09:23:07 -0700

    Add and pass Stylist#save and .all specs

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -4,4 +4,23 @@ attr_reader(:id, :name)
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
   end
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
+
+  define_singleton_method(:all) do
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    all_stylists = []
+    returned_stylists.each() do |stylist|
+      id = stylist.fetch('id').to_i()
+      name = stylist.fetch('name')
+      all_stylists.push(Stylist.new(:id => id, :name => name))
+    end
+    all_stylists
+  end
+
+  define_method(:==) do |another_stylist|
+    self.name().==(another_stylist.name()).&(self.id().==(another_stylist.id()))
+  end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,16 +1,28 @@
 require('spec_helper')
 
 describe(Stylist) do
-    describe('#id') do
-      it('returns the id of the stylist') do
-      test_stylist = Stylist.new(:id => 1, :name => "lynn")
-      expect(test_stylist.id()).to(eq(1))
-      end
+  describe('#id') do
+    it('returns the id of the stylist') do
+    test_stylist = Stylist.new(:id => 1, :name => "lynn")
+    expect(test_stylist.id()).to(eq(1))
     end
-    describe('#name') do
-      it('returns the name of the stylist') do
-      test_stylist = Stylist.new(:id => 1, :name => "lynn")
-      expect(test_stylist.name()).to(eq("lynn"))
+  end
+  describe('#name') do
+    it('returns the name of the stylist') do
+    test_stylist = Stylist.new(:id => nil, :name => "lynn")
+    expect(test_stylist.name()).to(eq("lynn"))
+    end
+  end
+  describe('.all') do
+    it('begins as an empty array') do
+    expect(Stylist.all()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it('saves the stylist to the database') do
+    test_stylist = Stylist.new(:id => nil, :name => "lynn")
+    test_stylist.save()
+    expect(Stylist.all()).to(eq([test_stylist]))
     end
   end
 end
